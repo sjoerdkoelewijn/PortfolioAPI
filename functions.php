c65e892278faabc153fdacb930021d0c351d811d
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-01-09 18:33:42 +0100

    removed background and header theme support

--- functions.php
+++ functions.php
@@ -4,8 +4,6 @@ add_theme_support( 'title-tag' );
 add_theme_support( 'menus' );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
-add_theme_support( 'custom-header' );
-add_theme_support( 'custom-background' );
 
 add_post_type_support('page', 'excerpt');
 
